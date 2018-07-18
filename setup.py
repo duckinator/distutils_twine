4c2422710da6b1ce41000ff861991f78915b60f2
Ellen Marie Dash <me@duckie.co>
2018-07-17 23:42:46 -0400

    well, that didn't work.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import setuptools
-#from distutils_twine import UploadCommand
+from distutils_twine import UploadCommand
 
-setuptools.setup()
-#setuptools.setup(cmdclass={"release": UploadCommand})
+setuptools.setup(cmdclass={"release": UploadCommand})
